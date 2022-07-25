310064c82b66cfe0278ee15e6f3456a677437f5f
Isidro Casiano <isidroICP@outlook.com>
2022-07-25 15:20:57 -0400

    feat: init (#1)
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt-get update && sudo apt-get install -y curl git zsh htop fzf gnupg tmux xclip
+
+stow bin
+stow nvim
+stow personal
+stow tmux
+stow zsh
+stow vscode
+
+sudo chsh -s $(which zsh) $USER
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+nvm use --lts
+npm install -g npm
+npm install -g typescript typescript-language-server
+
+nvim --headless +PlugInstall +qall
+
+npm install -g yarn
---
+++ nvim/.config/nvim/lua/juanpablo/init.lua
@@ -0,0 +1,1 @@
+require('juanpablo.lsp')
